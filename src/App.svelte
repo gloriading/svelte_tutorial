66897bca5f2edb720fff8b04a1e9ffd05e3a79f9
Gloria Ding <gloria.hc.ding@gmail.com>
2021-08-28 12:28:21 -0700

    Hello World with variable and style

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,26 @@
 <script>
-	export let name;
+  const name = 'Gloria';
+  const src = 'favicon.png';
+  let count = 0;
+
+  const increment = () => (count += 1);
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello {name}</h1>
+  <h3>Counter: {count}</h3>
+  <button on:click={increment}>Add 1 to counter</button>
+  <img {src} alt="Svelte" />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+  }
+  h1 {
+    text-align: center;
+  }
 </style>
