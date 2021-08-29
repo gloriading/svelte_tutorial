98c50e5d0995aa7243e114e58021253eb497b02e
Gloria Ding <gloria.hc.ding@gmail.com>
2021-08-29 09:16:36 -0700

    loop

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,13 @@
 
   const src = 'favicon.png';
 
+  // Loops
+  let fruits = [
+    { name: 'apple', color: 'red', id: 1 },
+    { name: 'banana', color: 'yellow', id: 2 },
+    { name: 'orange', color: 'orange', id: 3 },
+  ];
+
   // Reactive
   $: fullName = `${firstName} ${lastName}`;
   $: console.log(lastName);
@@ -17,6 +24,15 @@
   <input type="text" bind:value={firstName} />
   <label for="lastName">Last Name</label>
   <input type="text" bind:value={lastName} />
+
+  {#each fruits as fruit (fruit.id)}
+    <div class="card">
+      <h4>{fruit.name}</h4>
+      <p>{fruit.color}</p>
+    </div>
+  {:else}
+    <p>There is no fruit</p>
+  {/each}
 </main>
 
 <style>
@@ -28,5 +44,15 @@
   }
   h1 {
     text-align: center;
+  }
+  h4,
+  p {
+    margin: 4px 0;
+  }
+  .card {
+    margin-bottom: 2px;
+    padding: 10px;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    width: 400px;
   }
 </style>
